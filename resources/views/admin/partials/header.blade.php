7d220124201beea89a17b0abe73f2e2561853e64
tien.nguyen <tien.nguyen3@ntq-solution.com.vn>
2019-12-09 17:38:28 +0700

    Update logout icon;

--- resources/views/admin/partials/header.blade.php
+++ resources/views/admin/partials/header.blade.php
@@ -39,7 +39,7 @@
                 <a class="dropdown-item" href="{{ route('logout') }}"
                    onclick="event.preventDefault();
                                  document.getElementById('logout-form').submit();">
-                    <i class="fa fa-lock"></i> Logout
+                    <i class="fa fa-sign-out"></i> Logout
                 </a>
                     
                 <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
